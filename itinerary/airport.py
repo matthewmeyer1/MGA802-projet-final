1f4ca5e6eaced18966d4db483322fcc743da42eb
Matthew Meyer <meyermatthew62@gmail.com>
2025-06-13 20:23:36 -0400

    debut de litineraire . itinerary -> classe pour les fonctions de litineraire . itinerary_ui -> interface utilisateur . waypoints -> Classe waypoint . legs -> classe legs

---
+++ itinerary/airport.py
@@ -0,0 +1,5 @@
+class Airport:
+    def __init__(self, lat, lon, runway_length):
+        self.lat = lat
+        self.lon = lon
+        self.runway_length = runway_length
---
+++ itinerary/data_retrieval.py
@@ -0,0 +1,5 @@
+from airport import Airport
+
+def airport_data(icao, airport_list):
+
+    return
