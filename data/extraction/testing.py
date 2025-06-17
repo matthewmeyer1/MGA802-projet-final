c511727909485c5aaf67eab0c10ed5f2112c4452
Antoine Gingras <antoi.gingras@gmail.com>
2025-06-16 22:30:50 -0400

    Ajout de l'altitude dans le dataframe

--- data/extraction/testing.py
+++ data/extraction/testing.py
@@ -1,43 +1,11 @@
-import pymupdf # imports the pymupdf library
 import re
 
+text = "UTC-5(4)\\nElev 1250"  # This is a string containing: UTC-5(4)\nElev 1250
 
-def pdf_to_text(pdf_name):
-    doc = pymupdf.open(f"{pdf_name}.pdf") # open a document
-    text = []
-    for page in doc: # iterate the document pages
-        text.append(str(page.get_text())) # get plain text encoded as UTF-8
-    "".join(text)
-    print(text)
-    return str(text)
+pattern = r"UTC-5\(4\\nElev 1250"  # raw string regex pattern
 
-
-def extract_airport_info(text):
-    aeroport_name = re.findall("[QC]{2}.nC[A-Z0-9]{3}",text)
-    coord = re.findall(".nN[0-9 ]{8} W[0-9 ]{8}",text)
-
-
-    longitude = ["A"] * len(coord)
-    lattitude = ["A"] * len(coord)
-
-    for i in range(len(aeroport_name)):
-        aeroport_name[i] = aeroport_name[i][4:]
-
-    for i in range(len(coord)):
-        longitude[i] = coord[i][2:11]
-        lattitude[i] = coord[i][12:]
-
-    return aeroport_name, longitude, lattitude
-
-text = pdf_to_text("../ressources/pdf/cfs_qc")
-aeroport_name, longitude, lattitude = extract_airport_info(text)
-
-print(len(aeroport_name), aeroport_name)
-print(len(longitude), longitude)
-print(len(lattitude), lattitude)
-
-print(len(aeroport_name))
-print(len(longitude))
-print(len(lattitude))
-
-
+match = re.search(pattern, text)
+if match:
+    print("✅ Match found!")
+else:
+    print("❌ No match.")
