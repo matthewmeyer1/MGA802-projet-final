f204d41169dc779bcef5575747258ddcecca383c
Antoine Gingras <antoi.gingras@gmail.com>
2025-06-17 17:39:47 -0400

    Bugfix pour que les fonctions regex fonctionne pour tous les fichiers pdf

--- data/extraction/testing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.interpolate import RegularGridInterpolator
-
-def interpolation_function(altitude_before, altitude_after, temp_before, temp_after, rpm_bef, rpm_after, ):
-    altitude = np.array([altitude_before, altitude_after])
-    temperature = np.array([temp_before, temp_after])
-    rpm = np.array([rpm_bef, rpm_after])
-    inter_func = RegularGridInterpolator((altitude, rpm, temperature), y)
